18f74f779f6770a3d99ad3a27077fc8ace1433d7
Enjay Tarigan <tariganagc@gmail.com>
2024-08-19 16:19:09 +0700

    fix GreyHeader is not centered align

--- src/components/headers/GreyHeader.js
+++ src/components/headers/GreyHeader.js
@@ -1,101 +1,106 @@
-import React from 'react'
+import React from "react";
 import {
     View,
     Text,
     TouchableOpacity,
     StatusBar,
     StyleSheet,
-} from 'react-native'
-import { Ionicons, Feather } from '@expo/vector-icons'
-import { 
-    widthPercentageToDP as wp, 
-    heightPercentageToDP as hp 
-} from 'react-native-responsive-screen'
-
-
-
+} from "react-native";
+import { Ionicons, Feather } from "@expo/vector-icons";
+import {
+    widthPercentageToDP as wp,
+    heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 
 export default function GreyHeader({
     navigate,
-    navigateBack = 'Home',
-    navigateTo, 
-    title = 'Enter Title Here', 
+    navigateBack = "Home",
+    navigateTo,
+    title = "Enter Title Here",
     edit = false,
     hidden = true,
     additionalFunction,
-    params
-}){
-    async function callAllFunction(){
-        await additionalFunction()
-        navigate(navigateBack)
+    params,
+}) {
+    async function callAllFunction() {
+        await additionalFunction();
+        navigate(navigateBack);
     }
 
     return (
         <View style={styles.container}>
-            <StatusBar 
+            <StatusBar
                 barStyle="light-content"
                 translucent={true}
-                backgroundColor={'transparent'}
+                backgroundColor={"transparent"}
                 // hidden={hidden}
             />
             <View style={styles.touchableContent}>
                 <TouchableOpacity
-                    onPress={() => additionalFunction ? callAllFunction() : navigate(navigateBack)}>
-                    <View style={styles.content} >
-                        <Ionicons 
-                                name="arrow-back" 
-                                color="#fff" 
-                                size={25} 
-                                style={styles.backArrow} 
-                        />
-                        <Text style={styles.text}>{title}</Text>
-                    </View>
-                </TouchableOpacity>
-            </View>
-            {edit ? 
-                <TouchableOpacity
-                    onPress={() => navigate(navigateTo, params)}>
-                    <Feather 
-                        name="edit" 
-                        color="#DDDDDD" 
-                        size={20} 
-                        style={styles.edit}
+                    onPress={() =>
+                        additionalFunction
+                            ? callAllFunction()
+                            : navigate(navigateBack)
+                    }
+                    style={{ flexDirection: "row" }}
+                >
+                    {/* <View style={styles.content}> */}
+                    <Ionicons
+                        name="arrow-back"
+                        color="#fff"
+                        size={25}
+                        style={styles.backArrow}
                     />
+                    <Text style={styles.text}>{title}</Text>
+                    {/* </View> */}
                 </TouchableOpacity>
-             : null}   
+
+                {edit ? (
+                    <TouchableOpacity
+                        onPress={() => navigate(navigateTo, params)}
+                    >
+                        <Feather
+                            name="edit"
+                            color="#DDDDDD"
+                            size={20}
+                            style={styles.edit}
+                        />
+                    </TouchableOpacity>
+                ) : null}
+            </View>
         </View>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flexDirection: 'row',
-        height: hp('10%'),
-        width: '100%',
-        backgroundColor: '#2F2F2F',
-        justifyContent: 'space-between',
-        alignItems: 'flex-end',
-        paddingTop: 20
+        flexDirection: "row",
+        height: hp("10%"),
+        width: "100%",
+        backgroundColor: "#2F2F2F",
+        alignItems: "flex-end",
+        paddingTop: 20,
     },
 
     touchableContent: {
-        flexDirection: 'row'
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        flex: 1,
     },
     content: {
-        flexDirection: 'row', 
-        marginBottom: 10
+        flexDirection: "row",
     },
-    backArrow: { 
-        paddingHorizontal: 17.64
+    backArrow: {
+        paddingHorizontal: 17.64,
     },
 
     text: {
-        color: '#DDDDDD',
-        fontSize: 18
+        color: "#DDDDDD",
+        fontSize: 18,
     },
 
     edit: {
-        paddingRight: 20
-    }
-
-})
+        paddingRight: 20,
+    },
+});
